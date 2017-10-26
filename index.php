a8ed275d3807c3a5867427064fd035986235b951
suclearnub <suclearnub00@gmail.com>
2017-10-26 17:02:35 +0800

    rewrite

--- index.php
+++ index.php
@@ -3,22 +3,23 @@
   startSession();
   initPage($title="Login", $styleSheetName="index.css")
  ?>
-<div id="fullscreen_bg" class="fullscreen_bg">
+<div id="fullscreen_bg" class="fullscreen_bg"/>
 
-<form action="login.php" method="POST" class="form-signin">
-  <h1>
-    <h1 class="form-signin-heading text-muted">Sign In</h1>
-		<input type="text" class="form-control" placeholder="Email" required="" autofocus="">
-		<input type="password" class="form-control" placeholder="Password" required="">
-		<button class="btn btn-lg btn-primary btn-block" type="submit">
-			Sign In
-		</button>
-</form>
-<form action="forgot.php" style="form-signin">
-  <button class="btn btn-lg btn-warning btn-block" type="submit">
-    Forgot Password?
+<div class="container">
+
+<form target="login.php" method="post" class="form-signin">
+  <h1 class="form-signin-heading text-muted">Sign In</h1>
+  <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+  <input type="password" class="form-control" placeholder="Password" required="">
+  <button class="btn btn-lg btn-primary btn-block" type="submit">
+    Sign In
   </button>
 </form>
+<form target="forgot.php" class="form-signin">
+  <button class="btn btn-primary btn-block" type="submit">
+    Forgot Password
+  </button>
+</form>
+
 </div>
-</body>
 </html>
