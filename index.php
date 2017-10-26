42312785f8ab0823fb27926578d3e0d7e9548703
suclearnub <suclearnub00@gmail.com>
2017-10-26 17:10:26 +0800

    action

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <div class="container">
 
-<form target="login.php" method="post" class="form-signin">
+<form action="login.php" method="post" class="form-signin">
   <h1 class="form-signin-heading text-muted">Sign In</h1>
   <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
   <input type="password" class="form-control" placeholder="Password" required="">
@@ -15,10 +15,12 @@
     Sign In
   </button>
 </form>
-<form target="forgot.php" class="form-signin">
+<form action="signup.php" class="form-misc">
   <button class="btn btn-primary btn-block" type="submit">
     Sign Up
   </button>
+</form>
+<form action="forgot.php" class="form-misc">
   <button class="btn btn-warning btn-block" type="submit">
     Forgot Password?
   </button>
