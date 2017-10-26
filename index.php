99f6adc634ea4f3051a28afa20818d19cd409901
suclearnub <suclearnub00@gmail.com>
2017-10-26 16:48:49 +0800

    check if fixed

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 			Sign In
 		</button>
 </form>
-<form action="forgot.php" style="form-forgot">
+<form action="forgot.php" style="form-signin">
   <button class="btn btn-warning" type="submit">
     Forgot Password?
   </button>
