142312c74f1bb7122ff540f782fbef1a1883113b
suclearnub <suclearnub00@gmail.com>
2017-11-07 14:33:51 +0800

    navbar first try

--- home.php
+++ home.php
@@ -2,5 +2,7 @@
 	require_once('stdlib.php');
 	securePage();
 	$database = databaseConnect();
+	initPage($title='Homepage', $styleSheetName='');
+	drawNavBar($currentPage='Home');
 ?>
 <p>If you see this you are logged in</p>
