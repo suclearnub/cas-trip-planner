d7db69602d1b4382a59205b90f5d791b7148aa16
suclearnub <suclearnub00@gmail.com>
2017-11-07 14:29:18 +0800

    navbar draw test

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <?php
 	require_once('stdlib.php');
 	securePage();
+	$database = databaseConnect();
 ?>
 <p>If you see this you are logged in</p>
--- stdlib.php
+++ stdlib.php
@@ -14,8 +14,28 @@
           <body>\r\n");
 
   }
-  function drawNavBar() {
+  function drawNavBar($currentPage) {
     # Draws the navbar
+    echo('<nav class="navbar navbar-inverse navbar-static-top">\r\n
+	<div class="container-fluid">\r\n
+		<div class="navbar-header">\r\n
+			<a class="navbar-brand" href="#">CAS Trip Planner</a>\r\n
+		</div>\r\n
+	<ul class="nav navbar-nav">\r\n');
+  $sql = "SELECT * FROM menuItems";
+  if($results -> num_rows > 0) {
+    while($row = $results -> fetch_assoc()) {
+      if($row['text'] == $currentPage) {
+        echo('<li class="active"><a href="$row[\'target\']">Home <span class="glyphicon glyphicon-home"></span></a></li>');
+      }
+      else {
+        echo('<li><a href="$row[\'target\']">Home <span class="glyphicon glyphicon-home"></span></a></li>');
+      }
+    }
+    echo('
+    	</ul>
+	</div>
+</nav>');
   }
 
   function databaseConnect() {
@@ -28,6 +48,7 @@
     $database = new mysqli($serverName, $username, $password, $dbName);
     return $database;
   }
+}
 
   function startSession() {
     # Starts new session and inits variables
