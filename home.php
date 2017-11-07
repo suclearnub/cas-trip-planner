316d75c6278a173cfa8f032c92d9e37cfc169c6f
suclearnub <suclearnub00@gmail.com>
2017-11-07 14:49:22 +0800

    Add a database

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 	securePage();
 	$database = databaseConnect();
 	initPage($title='Homepage', $styleSheetName='');
-	drawNavBar($currentPage='Home');
+	drawNavBar($currentPage='Home', $database=$database);
 ?>
 <p>If you see this you are logged in</p>
 </body>
