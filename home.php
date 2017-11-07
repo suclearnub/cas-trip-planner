d72b7217577bf328aa1b362562b967962ea05967
suclearnub <suclearnub00@gmail.com>
2017-11-07 15:06:13 +0800

    fix parse error

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 <div class="container-fluid">
 <p>If you see this you are logged in</p>
 <?php
-	$results = $database -> query("SELECT firstName FROM users WHERE email=$_SESSION['email']");
+	$results = $database -> query("SELECT firstName FROM users WHERE email=$_SESSION[email]");
 	echo("<h2>Welcome back, $results</h2>")
 ?>
 </div>
