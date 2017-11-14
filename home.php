8a3fe446634c979c38d694a07e970aa7059edb24
suclearnub <suclearnub00@gmail.com>
2017-11-14 11:43:01 +0800

    Smash a bug

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 	securePage();
 	$database = databaseConnect();
 	initPage($title='Homepage', $styleSheetName='');
-	drawNavBar($currentPage='Home', $database=$database);
+	drawNavBar($currentPage='Home', $database);
 ?>
 <div class="container-fluid">
 <p>If you see this you are logged in</p>
@@ -13,7 +13,8 @@
 <h3>Here's what's going on:</h3>
 <h4>Your Trips</h4>
 <?php
-	$results = getQuery("SELECT * FROM userPermissions WHERE userNo=$_SESSION[userNo]", $database=$database);
+	$results = getQuery("SELECT * FROM userPermissions WHERE userNo=$_SESSION[userNo]", $database);
+	echo("SELECT * FROM userPermissions WHERE userNo=$_SESSION[userNo]");
 	while($row = $results -> fetch_assoc()) {
 		if($row['userPermissions'] == 0 || $row['permissionNo'] == 2) {
 			$trips = getQuery("SELECT * FROM trips");
