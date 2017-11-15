4437876255dfab4e511775528bc2eb1f173ccf4c
suclearnub <suclearnub00@gmail.com>
2017-11-15 12:16:12 +0800

    Add awaiting approval

--- home.php
+++ home.php
@@ -14,16 +14,38 @@
 <h4>Your Trips</h4>
 <?php
 	$results = getQuery("SELECT * FROM userPermissions WHERE userNo=$_SESSION[userNo]", $database);
-	echo($_SESSION['userNo']);
 	while($row = $results -> fetch_assoc()) {
 		if($row['userPermissions'] == 0 || $row['permissionNo'] == 2) {
-			# If they have unlimited power or permission no. 2, list all the trips
+			# If they have unlimited power or permission no. 2 (view_other_trips), list all the trips
 			drawTable("SELECT * FROM trips", $database, ["Trip ID", "Name", "Description", "Start Date", "End Date", "Approval"]);
 			}
+		else {
+			# Else, show all trips they are in.
+			drawTable("SELECT * from trips t join tripParticipants p on t.tripNo = p.tripNo WHERE p.userNo = 1", $database,  ["Trip ID", "Name", "Description", "Start Date", "End Date", "Approval"]);
+		}
 		}
  ?>
 <h4>Awaiting Approval</h4>
+	<?php
+		$results = getQuery("SELECT * FROM userPermissions WHERE userNo=$_SESSION[userNo]", $database);
+		while($row = $results -> fetch_assoc()) {
+			if($row['userPermissions'] == 0 || $row['permissionNo'] == 4) {
+				# If they have unlimited power or permission no. 4 (modify_other_trips), list all the trips that haven't been confirmed
+				drawTable("SELECT * FROM trips WHERE confirmed = 0", $database, ["Trip ID", "Name", "Description", "Start Date", "End Date", "Approval"]);
+			}
+			if ($row['userPermissions'] == 3) {
+				# Else, check if they have permission to modify_own_trips
+				# If they do, the have permission to edit all trips they are in
+				drawTable("SELECT * from trips t join tripParticipants p on t.tripNo = p.tripNo WHERE p.userNo = 1", $database,  ["Trip ID", "Name", "Description", "Start Date", "End Date", "Approval"]);
+			}
+			else {
+				# They are powerless to modify Trips
+				echo("<br>");
+			}
+		}
+	 ?>
 <h4>New Comments</h4>
+	<p>To be implemented.</p>
 </div>
 </body>
 </html>
