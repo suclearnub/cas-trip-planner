206eba6ba66bacf24ef3983a0c21e76c06162634
suclearnub <suclearnub00@gmail.com>
2017-11-21 13:16:24 +0800

    jquery fix

--- home.php
+++ home.php
@@ -46,5 +46,8 @@
 <h4>New Comments</h4>
 	<p>To be implemented.</p>
 </div>
+<script src="https://code.jquery.com/jquery.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jasny-bootstrap.min.js"></script>
 </body>
 </html>
