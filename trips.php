6644a056d226390da6d54a2e60850cefb64b7844
suclearnub <suclearnub00@gmail.com>
2017-11-21 15:17:37 +0800

    Table drawing bush smashing

--- trips.php
+++ trips.php
@@ -7,8 +7,7 @@ drawNavBar($currentPage='My Trips', $database);
 if (checkPermission($database, 0) || checkPermission($database, 2) || checkPermission($database, 4)) {
   # If this is true, they have unlimited power, view others or edit others -> show them any trip!
   echo("<div class='container-fluid'>");
-  drawTable("SELECT tripActivityNo, description, cost, confirmed, startDate, endDate FROM tripActivities WHERE tripNo = $_POST[id]", $database, ["Activity ID", "Description", "Cost", "Start Date", "End Date"], "activity", "tripActivityNo", "id");
-
+  drawTable("SELECT tripActivityNo, description, cost, confirmed, startDate, endDate FROM tripActivities WHERE tripNo = $_POST[id]", $database, ["Activity ID", "Description", "Cost", "Confirmation", "Start Date", "End Date"], "activity", "tripActivityNo", "id");
 }
 else {
   # Else, they can only see their own trips. Therefore, check if they're actually in the trip. they're requesting
