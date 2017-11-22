c88b416e82e31f3e249550b1f1ed1d216d088f7b
suclearnub <suclearnub00@gmail.com>
2017-11-22 12:47:12 +0800

    Change to use SQL to determine true or false

--- stdlib.php
+++ stdlib.php
@@ -114,13 +114,6 @@
       echo("<tr>\r\n");
       $i = 0;
       foreach($row as $rowElement) {
-        if (!$rowElement) {
-          # If the row element's value is 0, it should be shown to the user as a 'No'.
-          $rowElement = 'No';
-        }
-        if ($rowElement) {
-          $rowElement = 'Yes';
-        }
         if ($i == 0) {
           echo("<td><a href='$getTo.php?$getVarName=$row[$getTargetName]' class='lookNormal'>$rowElement</a></td>\r\n");
         } else {
