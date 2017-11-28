d18a2fb475f3407e6cd1f6b13fee6f5f53550094
suclearnub <suclearnub00@gmail.com>
2017-11-28 15:11:59 +0800

    Pass on spacing styling

--- trips.php
+++ trips.php
@@ -13,17 +13,20 @@ if (checkPermission($database, 0, $_SESSION['userNo']) || checkPermission($datab
   echo("<h2>Overview:</h2>");
   drawNonSQLTable([[sizeof(getParticipants($_GET['id'], $database)), 'Cost Placeholder', getConfirmation($_GET['id'], 'trips', $database)]], ['Participants', 'Cost', 'Confirmation']);
 
+  echo("<br><br>");
+
   echo("<h2>Participants</h2>");
   if(checkPermission($database, 0, $_SESSION['userNo']) || checkPermission($database, 7, $_SESSION['userNo']) || checkPermission($database, 8, $_SESSION['userNo'])) {
       drawTable("SELECT t.userNo, CONCAT(u.firstName, ' ', u.lastName) AS name, CASE WHEN confirmed = 1 THEN 'Yes' ELSE 'No' END AS confirmed, CASE WHEN passportOK = 1 THEN 'Yes' ELSE 'No' END AS passportOK, CASE WHEN visaOK = 1 THEN 'Yes' ELSE 'No' END AS visaOK, CASE WHEN paid = 1 THEN 'Yes' ELSE 'No' END AS paid FROM tripParticipants t JOIN users u ON t.userNo = u.userNo WHERE t.tripNo = 1
 ", $database, ['User ID', 'Name', 'Confirmed', 'Passport OK?', 'Visa OK?', 'Paid?'], 'profile', 'userNo', 'id');
   }
 
+  echo("<br><br>");
+
   echo("<h2>Activities</h2>");
-  echo("<h4>Confirmed Activities</h4>");
+  echo("<h4>Confirmed</h4>");
   drawTable("SELECT tripActivityNo, description, cost, CASE WHEN confirmed = 1 THEN 'Yes' ELSE 'No' END AS confirmed, startDate, endDate FROM tripActivities WHERE tripNo = $_GET[id] AND confirmed = True", $database, ["Activity ID", "Description", "Cost", "Confirmation", "Start Date", "End Date"], "activity", "tripActivityNo", "id");
-  echo("<br>");
-  echo("<h4>Activities Pending Approval</h4>");
+  echo("<h4>Pending</h4>");
   drawTable("SELECT tripActivityNo, description, cost, CASE WHEN confirmed = 1 THEN 'Yes' ELSE 'No' END AS confirmed, startDate, endDate FROM tripActivities WHERE tripNo = $_GET[id] AND confirmed = False", $database, ["Activity ID", "Description", "Cost", "Confirmation", "Start Date", "End Date"], "activity", "tripActivityNo", "id");
 
 }
