c38747c013c77037304a15401497397c4b1daf92
Anson <git@anson-cheung.com>
2017-12-15 23:36:13 +0800

    More dangling divs fixed with the w3c validator

--- stdlib.php
+++ stdlib.php
@@ -364,16 +364,15 @@
       echo("</select>
             <input type='hidden' name='confirmed' value='False'>");
     }
-    echo("
+    echo("</div>
           <div class='modal-footer'>
             <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
             <button type='submit' class='btn btn-primary'>Save changes</button>
+          </div>
             </form>
           </div>
-          </div>
         </div>
-      </div>
-    </div>");
+      </div>");
   }
 
  ?>
