4e55a809c16686f999a35eb778b1d97441bd86cc
Anson <git@anson-cheung.com>
2017-12-16 00:07:12 +0800

    Add javascript accordinly

--- modals.js
+++ modals.js
@@ -1,23 +1,7 @@
 $(function () {
     $('#datetimepicker1').datetimepicker();
-    $('#datetimepicker2').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker1").on("dp.change", function (e) {
-        $('#datetimepicker2').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker2").on("dp.change", function (e) {
-        $('#datetimepicker1').data("DateTimePicker").maxDate(e.date);
-    });
-
+    $('#datetimepicker2').datetimepicker();
     $('#datetimepicker3').datetimepicker();
-    $('#datetimepicker4').datetimepicker({
-        useCurrent: false //Important! See issue #1075
-    });
-    $("#datetimepicker3").on("dp.change", function (e) {
-        $('#datetimepicker4').data("DateTimePicker").minDate(e.date);
-    });
-    $("#datetimepicker4").on("dp.change", function (e) {
-        $('#datetimepicker3').data("DateTimePicker").maxDate(e.date);
-    });
+    $('#datetimepicker4').datetimepicker();
+
 });
