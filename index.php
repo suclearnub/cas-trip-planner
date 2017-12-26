25d363633bdd547699475b8d32b9b3e861a33b24
Anson <git@anson-cheung.com>
2017-12-27 00:21:11 +0800

    Add sign up modal

--- index.php
+++ index.php
@@ -15,11 +15,33 @@
     Sign in
   </button>
 </form>
-<form action="signup.php" class="form-misc">
-  <button class="btn btn-primary btn-block" type="submit">
-    Sign Up
-  </button>
-</form>
+
+    <button type='button' class="btn btn-primary btn-block" data-toggle='modal' data-target='#myModalSignUp'>
+        Sign up
+    </button>
+    <div class='modal fade' id='myModalSignUp' tabindex='-1' role='dialog' aria-labelledby='myModalLabel'>
+        <div class='modal-dialog' role='document'>
+            <div class='modal-content'>
+                <div class='modal-header'>
+                    <button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>
+                    <h4 class='modal-title' id='myModalLabel'>Sign up</h4>
+                </div>
+                <div class='modal-body'>
+                    <form action='signup.php' method='post' class='form-signin'>
+                        <input type='text' class='form-control' placeholder='Email address' required='True' autofocus='' name='description'><br>
+                        <input type='text' class='form-control' placeholder='Password' required='True' autofocus='' name='cost'><br>
+                        <input type='text' class='form-control' placeholder='Recovery phrase' required='True' autofocus='' name='phrase'><br>
+
+                        <div class='modal-footer'>
+                            <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                            <button type='submit' class='btn btn-primary'>Save changes</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
 <form action="forgot.php" class="form-misc">
   <button class="btn btn-warning btn-block" type="submit">
     Forgot Password?
