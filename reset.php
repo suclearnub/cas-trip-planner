8a9b08903b3dc4d9848ebe318ba91e18ddefe24e
suclearnub <suclearnub00@gmail.com>
2018-01-09 14:53:26 +0800

    Fix quotation marks in SQL statement

--- reset.php
+++ reset.php
@@ -2,5 +2,5 @@
 require_once('stdlib.php');
 $database = databaseConnect();
 $pwhash = hash('sha256', $_POST['password']);
-$results = getQuery("UPDATE users SET password = '$pwhash' WHERE email = $_POST[email] AND recoveryPhrase = $_POST[phrase]", $database);
+$results = getQuery("UPDATE users SET password = '$pwhash' WHERE email = '$_POST[email]' AND recoveryPhrase = '$_POST[phrase]'", $database);
 echo("<a href='index.php'>Password reset request has been submitted. Your password has been updated. Click here to continue...</a>");
