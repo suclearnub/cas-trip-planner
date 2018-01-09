b69a9f7627ef292760d9bb422b67b65a2efe33c1
suclearnub <suclearnub00@gmail.com>
2018-01-09 14:34:42 +0800

    Fix issues with inserting new user

--- signup.php
+++ signup.php
@@ -2,5 +2,5 @@
 require_once('stdlib.php');
 $database = databaseConnect();
 $pwhash = hash('sha256', $_POST['password']);
-$results = getQuery("INSERT INTO users (firstName, lastName, gender, email, password, attempts, isTeacher, recoveryPhrase VALUES ($_POST[firstName], $_POST[lastName], $_POST[gender], $_POST[email], $pwhash, 0, 0, $_POST[phrase])", $database);
+$results = getQuery("INSERT INTO users (firstName, lastName, gender, email, password, attempts, isTeacher, recoveryPhrase) VALUES ('$_POST[firstName]', '$_POST[lastName]', '$_POST[gender]', '$_POST[email]', $pwhash, 0, 0, '$_POST[phrase]')", $database);
 echo("<a href='index.php'>Account creation success. Click here to return and log in...</a>");
